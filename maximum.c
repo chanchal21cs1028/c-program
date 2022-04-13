f0cd0fec98df3a8a37daf604e9858c7b84e2cd22
chanchal21cs1028 <98004293+chanchal21cs1028@users.noreply.github.com>
2022-04-13 19:52:56 +0530

    Add files via upload

---
+++ maximum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int n1,n2,n3;
+    printf("Enter three numners:");
+    scanf("%d %d %d",&n1,&n2,&n3);
+    if(n1>n2&&n1>n3)
+       printf("%d is maximum",n1);
+    else if(n2>n1&&n2>n3)
+       printf("%d is maximum",n2);
+    else
+       printf("%d is maximum",n3);       
+
+
+
+}
